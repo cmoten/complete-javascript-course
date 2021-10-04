9425daadfaa667c5eff1c6b7779b0d44c21b92ac
cmoten <cmoten@gmail.com>
2021-10-04 21:28:24 +0200

    completed binding lesson

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,215 @@
 'use strict';
+/*
+//////////////////////////////
+//Functions-Call, Apply, and Bind
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Cardy Moten');
+lufthansa.book(635, 'Angelina Moten');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+//Does not work
+// book(23, 'Sarah Williams');
+
+//Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Charlie Brown');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+book.call(swiss, 583, 'Mary Cooper');
+
+//Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+//Better Method
+book.call(swiss, ...flightData);
+
+//Bind Method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Stephen Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Cardy Moten');
+bookEW23('Angelina Moten');
+
+//With Event Listners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+
+const addVAT = addTax.bind(null, 0.23);
+//addVAT = value => value + value * 0.23
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+//////////////////////////////
+
+//////////////////////////////
+//Functions-Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Cardy');
+greeterHey('Angelina');
+
+greet('Hello')('Cardy');
+
+//Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Cardy');
+//////////////////////////////
+
+
+//////////////////////////////
+//Functions-Callbak Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('\u{1F44B}');
+};
+
+document.body.addEventListener('click', high5);
+
+['Cardy', 'Angelina', 'Billy'].forEach(high5);
+//////////////////////////////
+
+
+//////////////////////////////
+//Functions-Passing Arguments
+const flight = 'LH234';
+const cardy = {
+  name: 'Cardy Moten',
+  passport: 2369745123,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2369745123) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+// checkIn(flight, cardy);
+// console.log(flight);
+// console.log(cardy);
+
+// //Is the same as doing...
+// const flightNum = flight;
+// const passenger = cardy;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(cardy);
+checkIn(flight, cardy);
+
+//////////////////////////////
+
+
+//////////////////////////////
+//Functions-Default Parameters
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //   ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+//////////////////////////////
+*/
